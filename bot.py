0425ed1628a065d5045113963e078617de8a63d1
Xalion <xalion67@gmail.com>
2020-06-07 17:44:05 +0300

    change text and add handler start

--- bot.py
+++ bot.py
@@ -6,9 +6,14 @@ import api
 bot = tb.TeleBot('937410305:AAEf-I48FEuMcjnK62YIwriUYFka4f8hLMU', threaded=False)
 
 
+@bot.message_handler(commands=['start'])
+def say_hello(message):
+    bot.send_message(message.chat.id, f'Привет, {message.user.username}./n Я умею переводить голосовые сообщения и аудио в текст.\nДля этого отправь мне что-то из этого)
+
+
 @bot.message_handler(content_types=['text'])
 def test(message):
-    bot.send_message(message.chat.id, 'очень большие сиськи и очень большие жопы')
+    bot.send_message(message.chat.id, 'Я ожидал получить голосовое сообщение или аудио')
 
 
 @bot.message_handler(content_types=['voice'])
